983299b96097d53eefbec309d4ed116790f46ed9
@Odisseu93 - Ulisses Silvério <contato@ulisses.tec.br>
2023-11-15 21:20:09 -0300

    feat: add `thumb` field in episodes

--- src/dtos/epsodio.dto.ts
+++ src/dtos/epsodio.dto.ts
@@ -4,12 +4,14 @@ export class EpisodeDTO {
   public seasonId: string
   public seriesId: string
   public name: string
+  public thumb?: string
 
-  constructor({ id, name, number, seasonId, seriesId: serieId }: EpisodeDTO) {
+  constructor({ id, name, thumb, number, seasonId, seriesId }: EpisodeDTO) {
     this.id = id
     this.name = name
+    this.thumb = thumb
     this.number = number
     this.seasonId = seasonId
-    this.seriesId = serieId
+    this.seriesId = seriesId
   }
 }
