4e1acf0abaab1ad6b49feaec1474e50cc2c89a09
@Odisseu93 - Ulisses Silvério <contato@ulisses.tec.br>
2023-11-20 18:16:57 -0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 
 import dotenv from 'dotenv'
 import seriesRoutes from './routes/series.routes'
@@ -7,6 +8,8 @@ dotenv.config()
 const app = express()
 
 const port = process.env.PORT || 3000
+
+app.use(cors())
 
 app.use('/', seriesRoutes)
 
