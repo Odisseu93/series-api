a0ed3db85cb4ba58ee3bf49fbe677c99647a7853
@Odisseu93 - Ulisses Silvério <contato@ulisses.tec.br>
2023-11-20 12:06:49 -0300

    feat: add series controller

---
+++ src/controllers/series.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express'
+import { Query } from 'express-serve-static-core'
+import { addSeries } from '../helpers/addSeries'
+
+import path from 'path'
+import fs from 'fs'
+
+interface TypedQuery<T extends Query> extends Request {
+  query: T
+}
+
+export class SeriesController {
+  static async getSeriesByName(
+    req: TypedQuery<{ name: string }>,
+    res: Response
+  ) {
+    const { name } = req.query
+
+    const dirpath = path.join(__dirname, `../mock/series/${name}`)
+
+    if (!fs.existsSync(dirpath))
+      res.status(404).json({ messge: 'Series not found!' })
+
+    if (name) {
+      try {
+        const result = addSeries(name, dirpath)
+
+        res.status(200).send(await result)
+      } catch (error) {
+        res.status(500).json({ message: error })
+      }
+    }
+  }
+}
