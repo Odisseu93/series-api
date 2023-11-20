759cd03336c07e5eefe9d309a02a11f71d2b4bde
@Odisseu93 - Ulisses Silvério <contato@ulisses.tec.br>
2023-11-20 12:44:59 -0300

    fix: change API root route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import express, { Request, Response } from 'express'
-
 import dotenv from 'dotenv'
 import seriesRoutes from './routes/series.routes'
 
@@ -8,8 +6,6 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-app.use('/series-api', seriesRoutes)
-
-app.get('/', (req: Request, res: Response) => res.send('Hello Series API!'))
+app.use('/', seriesRoutes)
 
 app.listen(port, () => console.log(` listen on http://localhost:${port}`))
